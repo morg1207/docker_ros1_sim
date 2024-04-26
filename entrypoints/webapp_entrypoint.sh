cec8ff1b224c4391fd8e6415efe129b87106a1bf
morg1207 <alaurao@uni.pe>
2024-04-26 02:56:06 +0000

    the web launch is launched automatically

--- entrypoints/webapp_entrypoint.sh
+++ entrypoints/webapp_entrypoint.sh
@@ -7,4 +7,8 @@ source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
 cd /catkin_ws/src/webpage_ws 
 
-python -m http.server 7000 
+python -m http.server 7000 &
+
+roslaunch course_web_dev_ros project.launch &
+
+wait
